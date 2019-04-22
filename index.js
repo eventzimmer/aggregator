2c2beb429b89ee66af2cd592c63886e56bf4a250
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-04-22 21:03:10 +0200

    Add tool test suite and standard linting.

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const process = require('process')
 const bunyan = require('bunyan')
 
 global.logger = bunyan.createLogger({
-    name: '@eventzimmer/aggregator',
-    level: (process.env.LOG_LEVEL !== undefined) ? process.env.LOG_LEVEL : 'debug'
+  name: '@eventzimmer/aggregator',
+  level: (process.env.LOG_LEVEL !== undefined) ? process.env.LOG_LEVEL : 'debug'
 })
 
 process.on('exit', () => {
-    global.logger.debug('Shutting down')
+  global.logger.debug('Shutting down')
 })
 
 const argv = require('yargs') // eslint-disable-line
-    .commandDir('./cmds')
-    .demandCommand()
-    .help()
-    .strict()
-    .argv
+  .commandDir('./cmds')
+  .demandCommand()
+  .help()
+  .strict()
+  .argv
