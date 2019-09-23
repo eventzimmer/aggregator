866963db3bc5c75eb7d34e0d1cdd0356fac41699
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-09-23 18:52:44 +0200

    Refactor facebook transformer to use async/await.

--- src/facebook.js
+++ src/facebook.js
@@ -1,7 +1,6 @@
 const { recordResponses } = require('./puppeteer')
 const GRAPHQL_URL = 'https://www.facebook.com/api/graphql/'
 const EVENT_URL = 'https://facebook.com/events/'
-// const SCONTENT_URL = 'https://scontent'
 
 /**
  * Sends a request to Facebook retrieving a list of events for the page
@@ -19,31 +18,28 @@ exports.loadFromSource = loadFromSource
  * @param {Object} source
  * @return {Promise<Array<object>>}
  */
-function transFormToEventList (source) {
-  return new Promise((resolve) => {
-    let edges = source.filter((s) => {
-      return (s.url === GRAPHQL_URL)
-    }).filter((s) => {
-      return (s.data.data.page !== undefined)
-    }).filter((s) => {
-      return (s.data.data.page.upcoming_events || s.data.data.page.upcomingRecurringEvents)
-    }).map((s) => {
-      return (s.data.data.page.upcoming_events) ? s.data.data.page.upcoming_events.edges : s.data.data.page.upcomingRecurringEvents.edges
-    }).reduce(function (a, b) {
-      return a.concat(b)
-    }, []).filter((e) => (typeof (e.node.time_range) !== 'undefined'))
-
-    resolve(edges.map((edge) => {
-      return {
-        id: edge.node.id,
-        time_range: edge.node.time_range,
-        event_place: edge.node.event_place,
-        is_canceled: edge.node.is_canceled,
-        name: edge.node.name,
-        url: `${EVENT_URL}${edge.node.id}`,
-        is_past: edge.node.is_past
-      }
-    }))
+async function transFormToEventList (source) {
+  let edges = source.filter((s) => {
+    return (s.url === GRAPHQL_URL)
+  }).filter((s) => {
+    return (s.data.data.page !== undefined)
+  }).filter((s) => {
+    return (s.data.data.page.upcoming_events || s.data.data.page.upcomingRecurringEvents)
+  }).map((s) => {
+    return (s.data.data.page.upcoming_events) ? s.data.data.page.upcoming_events.edges : s.data.data.page.upcomingRecurringEvents.edges
+  }).reduce(function (a, b) {
+    return a.concat(b)
+  }, []).filter((e) => (typeof (e.node.time_range) !== 'undefined'))
+  return edges.map((edge) => {
+    return {
+      id: edge.node.id,
+      time_range: edge.node.time_range,
+      event_place: edge.node.event_place,
+      is_canceled: edge.node.is_canceled,
+      name: edge.node.name,
+      url: `${EVENT_URL}${edge.node.id}`,
+      is_past: edge.node.is_past
+    }
   })
 }
 
@@ -55,36 +51,17 @@ exports.transFormToEventList = transFormToEventList
  * @param {object} source
  * @return {Promise<object>}
  */
-function transFormToEventDetails (source) {
-  return new Promise((resolve, reject) => {
-    /* let images = source.filter((s) => {
-      return (s.url.startsWith(SCONTENT_URL))
-    }) */
-
-    let queries = source.filter((s) => {
-      return (s.url === GRAPHQL_URL)
-    }).map((s) => {
-      return s.data
-    }).filter((body) => {
-      return (body.data.event !== undefined)
-    }).filter((body) => {
-      return (body.data.event.details !== undefined)
-    })
-
-    // Luckily enough the title image will always be loaded first because all the other images are affiliated links.
-    // Thanks Facebook for making user friendly, easy-to-predict apps (:
-    if (queries.length) {
-      resolve({
-        description: queries.shift().data.event.details.text
-        // images: [images.shift().url]
-      })
-    } else {
-      resolve({
-        description: ''
-        // images: [images.shift().url]
-      })
-    }
+async function transFormToEventDetails (source) {
+  let queries = source.filter((s) => {
+    return (s.url === GRAPHQL_URL)
+  }).map((s) => {
+    return s.data
+  }).filter((body) => {
+    return (body.data.event !== undefined)
+  }).filter((body) => {
+    return (body.data.event.details !== undefined)
   })
+  return (queries.length) ? { description: queries.shift().data.event.details.text } : { description: '' }
 }
 
 exports.transFormToEventDetails = transFormToEventDetails
@@ -93,15 +70,15 @@ exports.transFormToEventDetails = transFormToEventDetails
  * Takes a edge object and returns an event
  * @function
  * @param {Object} data
- * @return {Promise<EventType>}
+ * @return {Promise<Object>}
  */
-function transFormToEvent (data) {
-  return Promise.resolve({
+async function transFormToEvent (data) {
+  return {
     name: data.name,
     starts_at: new Date(data.time_range.start),
     url: `${EVENT_URL}${data.id}`,
     location: data.event_place.name
-  })
+  }
 }
 
 exports.transFormToEvent = transFormToEvent
