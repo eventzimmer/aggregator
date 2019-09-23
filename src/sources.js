747c9d2e4d9d200e84a5c0c3de5f0dc1d9228409
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-09-23 18:51:41 +0200

    Refactor sources to use async/await.

--- src/sources.js
+++ src/sources.js
@@ -7,33 +7,23 @@ const { promisify } = require('util')
  * @function
  * @return {Promise<Object>}
  */
-function currentSource () {
-  return new Promise((resolve, reject) => {
-    const client = createClient()
-    const llenAsync = promisify(client.llen).bind(client)
-    const lpushAsync = promisify(client.lpush).bind(client)
-    const lpopAsync = promisify(client.lpop).bind(client)
+async function currentSource () {
+  const client = createClient()
+  const llenAsync = promisify(client.llen).bind(client)
+  const lpushAsync = promisify(client.lpush).bind(client)
+  const lpopAsync = promisify(client.lpop).bind(client)
 
-    llenAsync('sources').then((response) => {
-      if (response === 0) {
-        return customHeaderRequest({
-          url: SOURCES_URL,
-          json: true
-        })
-          .then((sources) => lpushAsync('sources', ...sources.map((s) => JSON.stringify(s))))
-      } else {
-        return Promise.resolve()
-      }
-    }).then(() => {
-      return lpopAsync('sources')
-    }).then((source) => {
-      client.quit()
-      resolve(source)
-    }).catch((err) => {
-      client.quit()
-      reject(err)
+  const response = await llenAsync('sources')
+  if (response === 0) {
+    let sources = await customHeaderRequest({
+      url: SOURCES_URL,
+      json: true
     })
-  })
+    await lpushAsync('sources', ...sources.map((s) => JSON.stringify(s)))
+  }
+  let source = await lpopAsync('sources')
+  client.quit()
+  return source
 }
 
 exports.currentSource = currentSource
