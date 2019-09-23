cd2008541950b16aad63c466a66c5707ba0d9862
Lennard Berger <lennard.berger@student.uni-tuebingen.de>
2019-09-23 18:56:11 +0200

    Refactor endpoint to use async/await.

--- src/endpoint.js
+++ src/endpoint.js
@@ -11,34 +11,25 @@ const AUTH_ENDPOINT = 'https://eventzimmer.eu.auth0.com/oauth/token'
  * @param {Array<Object>} events
  * @return {Promise<any>}
  */
-function createEvents (events) {
-  return new Promise((resolve, reject) => {
-    const client = createClient()
-    const getAsync = promisify(client.get).bind(client)
-    getAsync('access_token')
-      .then((token) => {
-        console.log(token)
-        return request.post(`${ENDPOINT_URL}/events`, {
-          auth: {
-            bearer: token
-          },
-          method: 'POST',
-          body: JSON.stringify(events),
-          headers: { 'Content-Type': 'application/json' },
-          resolveWithFullResponse: true
-        })
-      }).then((response) => {
-        if (response.statusCode === 201) {
-          resolve(response.body)
-        } else {
-          reject(response.body)
-        }
-      }).catch((err) => {
-        reject(err)
-      }).finally(() => {
-        client.quit()
-      })
+async function createEvents (events) {
+  const client = createClient()
+  const getAsync = promisify(client.get).bind(client)
+  const token = await getAsync('access_token')
+  client.quit()
+  const response = await request.post(`${ENDPOINT_URL}/events`, {
+    auth: {
+      bearer: token
+    },
+    method: 'POST',
+    body: JSON.stringify(events),
+    headers: { 'Content-Type': 'application/json' },
+    resolveWithFullResponse: true
   })
+  if (response.statusCode === 201) {
+    return response.body
+  } else {
+    throw new Error(response.body)
+  }
 }
 
 exports.createEvents = createEvents
@@ -49,12 +40,9 @@ exports.createEvents = createEvents
  * @function
  * @return {Promise<string> | Promise<null>}
  */
-function requestToken () {
+async function requestToken () {
   if (process.env.CLIENT_ID && process.env.CLIENT_SECRET) {
-    const client = createClient()
-    const setAsync = promisify(client.set).bind(client)
-
-    return request.post({
+    const response = await request.post({
       url: AUTH_ENDPOINT,
       json: {
         client_id: process.env.CLIENT_ID,
@@ -62,18 +50,17 @@ function requestToken () {
         audience: 'api.eventzimmer.de',
         grant_type: 'client_credentials'
       }
-    }).then((body) => {
-      const token = body.access_token
-      return setAsync('access_token', token, 'EX', 35000)
-    }).then((status) => {
-      client.quit()
-      throw (new Error(status))
-    }).catch((err) => {
-      client.quit()
-      return Promise.reject(err)
     })
+    if (response.status === 200) {
+      const client = createClient()
+      const setAsync = promisify(client.set).bind(client)
+      await setAsync(response.body.access_token)
+      client.quit()
+    } else {
+      throw new Error(response.statusText)
+    }
   } else {
-    return Promise.reject(new Error('CLIENT_ID or CLIENT_SECRET not specified!'))
+    throw new Error('CLIENT_ID or CLIENT_SECRET not specified!')
   }
 }
 
